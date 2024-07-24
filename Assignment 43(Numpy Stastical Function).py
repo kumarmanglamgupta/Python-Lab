03e1fd45a31c7dbcdc5556efe80cd9419c4cd158
kumarmanglamgupta <therobotboy3@gmail.com>
2024-07-24 16:47:52 +0530

    Add files via upload

---
+++ Assignment 43(Numpy Stastical Function).py
@@ -0,0 +1,30 @@
+"""
+1.Compute the median of the flattened NumPy array 
+
+Input: x_odd = np.array([1, 2, 3, 4, 5, 6, 7])
+"""
+
+# importing numpy as library
+import numpy as np
+
+# Input array
+x_odd = np.array([1, 2, 3, 4, 5, 6, 7])
+print("\nPrinting the original array:")
+print(x_odd)
+
+# Calculate the median
+median = np.median(x_odd)
+print ("\nMedian of the array that contains print odd no elements:")
+# Output the result
+print(median)
+
+
+
+"""
+Output:
+Printing the original array:
+[1 2 3 4 5 6 7]
+
+Median of the array that contains print odd no elements:
+4.0
+"""
---
+++ Assignment 44(Numpy Stastical Function).py
@@ -0,0 +1,32 @@
+"""
+2.Compute the standard deviation of the NumPy array
+
+Input: arr = [20, 2, 7, 1, 34]
+"""
+
+# importing numpy as library
+# numpy.std() method
+import numpy as np
+#1D array
+arr = [20, 2, 7, 1, 34]
+print("arr : ",  arr)
+print("std of arr : ", np.std(arr))
+
+print("\nMore precision with float32")
+print("std of arr : ", np.std(arr, dtype= np.float32))
+
+print("\nMore accuracy with float64")
+print("std of arr : ", np.std(arr, dtype= np.float64))
+
+
+"""
+Output:
+arr :  [20, 2, 7, 1, 34]
+std of arr :  12.576167937809991
+
+More precision with float32
+std of arr :  12.576168
+
+More accuracy with float64
+std of arr :  12.576167937809991
+"""
